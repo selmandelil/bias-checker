f0ff5a3c9a9711d71684281ffe96d5433489bf9d
Selman Delil <selman.delil@wawlabs.com>
2020-07-25 00:14:44 +0300

    converted to a class and comments added

--- lenght-bias.py
+++ lenght-bias.py
@@ -1,13 +1,26 @@
 
-import numpy as np
+"""
+e.g.
+Average accross classes:
+145 classes with 1 students => 145 student
+334 classes with 100+ students => 33400 student
 
-def lenght_biased_sampling(sample, weight):
-    """
-    Generate a biased sammple using weight of an unbiased sample.
-    sample : numpy array
-    weight : numpy array same size with sample.
-    """
-    n = len(sample)
-    p = weight / np.sum(weight)
-    return np.random.choice(sample, n, p=p)
+Large classes get oversampled
+
+Class size x gets oversampled by x
+
+"""
+
+
+import numpy as np
+class BiasChecker:
+    def lenght_biased_sampling(self, sample, weight):
+        """
+        Generate a biased sammple using weight of an unbiased sample.
+        sample : unbiased sample as a numpy array
+        weight : a weight of sample as a numpy array same size with sample. It can be sample itself!
+        """
+        n = len(sample)
+        p = weight / np.sum(weight)
+        return np.random.choice(sample, n, p=p)
 
